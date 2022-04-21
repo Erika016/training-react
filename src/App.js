bb2e97989eb04e72f6c7b003b9f3bffdccac7d05
erika <ericadominguezbarrientos@gmail.com>
2022-04-21 20:52:51 +0200

    exercise react

---
+++ src/App.js
@@ -0,0 +1,10 @@
+// import logo from "./logo.svg";
+import "./App.css";
+// importo Nombre del archivo Nombre
+import Nombre from "./Nombre";
+
+function App() {
+  return <Nombre />;
+}
+// retorna la funcion
+export default App;
---
+++ src/Nombre.js
@@ -0,0 +1,11 @@
+// import './Nombre.css';
+function Nombre() {
+  return (
+    <div>
+      <h1>Erika Dominguez</h1>
+      <p>Hello world</p>
+    </div>
+  );
+}
+// el expor default llama la funcion para hacer el import en app.jss
+export default Nombre;
